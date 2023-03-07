d8a928b12acfb4254b50a9592a0a0ea82b9f9b7c
KollihTheGuruh <mmike8449@gmail.com>
2023-03-07 15:32:38 -0800

    strings

---
+++ practice/strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main()
+{
+    char userName[20];
+
+    printf("Enter your username:");
+    scanf("%s", userName);
+
+    printf("Your username is: %s\n", userName);
+}
