e39a102b1630c381ab9da0d8e5c7c8d53309c323
KollihTheGuruh <mmike8449@gmail.com>
2023-11-22 07:44:56 +0300

    laptop bazaar shop backend

---
+++ Project/laptopbazaar/mainapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path('', views.home, name='home'),
+    path('login/', views.login_view, name='login_view'),
+    path('dashboard/', views.dashboard_view, name='dashboard_view'),
+    path('products/', views.product_view, name='product_view'),
+    path('compare/', views.compare_view, name='compare_view'),
+    path('checkout/', views.checkout_view, name='checkout_view'),
+    path('payment/', views.payment_view, name='payment_view'),
+    # Add other URL patterns as needed
+]
---
+++ Project/laptopbazaar/mainapp/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import Laptop, User, Order
+# Add any other necessary imports
+
+# Login View
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('dashboard')
+        else:
+            # Return an 'invalid login' error message.
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    else:
+        return render(request, 'login.html')
+
+# Dashboard View
+def dashboard_view(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    # Additional logic based on user type (superuser, client, customer)
+    return render(request, 'dashboard.html')
+
+# Product View
+def product_view(request):
+    laptops = Laptop.objects.all()
+    return render(request, 'product_list.html', {'laptops': laptops})
+
+# Compare View
+def compare_view(request):
+    # This will depend on how you're planning to implement the comparison feature.
+    # For instance, you might use query parameters to get the IDs of laptops to compare.
+    return render(request, 'compare.html')
+
+# Checkout View
+def checkout_view(request):
+    if request.method == "POST":
+        # Process the checkout form
+        # Create an order instance, etc.
+        return redirect('payment')
+    else:
+        # Display checkout form
+        return render(request, 'checkout.html')
+
+# Payment View
+def payment_view(request):
+    if request.method == "POST":
+        # Process payment here
+        # Integrate with M-Pesa or other payment gateways
+        return redirect('success')
+    else:
+        return render(request, 'payment.html')
